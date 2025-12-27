8379c51317b39be45f0e9717846ca6bbbf50cfa7
Harikrishna D <harikrishnad2006@gmail.com>
2025-12-27 12:53:46 +0530

    Delete springapp directory

--- springapp/src/main/java/com/examly/springapp/exception/GlobalExceptionHandler.java
+++
@@ -1,7 +0,0 @@
-package com.examly.springapp.exception;
-
-import org.springframework.web.bind.annotation.RestControllerAdvice;
-
-@RestControllerAdvice
-public class GlobalExceptionHandler {
-}
--- springapp/src/main/java/com/examly/springapp/service/TaskAssignmentService.java
+++
@@ -1,8 +0,0 @@
-package com.examly.springapp.service;
-
-import java.util.List;
-import com.examly.springapp.model.TaskAssignment;
-
-public interface TaskAssignmentService {
-    List<TaskAssignment> getByUser(Long userId);
-}
